6195b10f601b13f9c9052ce74c2cf82ee76b92e4
Gokul nath <gokulnath634@gmail.com>
2020-07-22 22:49:28 +0530

    Getting started with calendar API

---
+++ Calender_API.py
@@ -0,0 +1,9 @@
+from apiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+
+scopes = ["https://www.googleapis.com/auth/calendar"]
+
+flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
+flow.run_console()
+
